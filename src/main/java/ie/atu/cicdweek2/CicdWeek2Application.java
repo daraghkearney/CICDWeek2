0c27eb8eee2b49c4cbf1ce82a92b8edc17f0b566
DARAGHKEARNEY-STUDEN <g00438215@atu.ie>
2025-09-24 14:31:45 +0100

    Setting up springboot project

---
+++ src/main/java/ie/atu/cicdweek2/CicdWeek2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdweek2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdWeek2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdWeek2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/cicdweek2/CicdWeek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdweek2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdWeek2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
